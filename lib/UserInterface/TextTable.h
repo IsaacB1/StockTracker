cbee93f0dd7f8c5140d75f9c6962723dbe3aed89
= <=>
2026-02-15 17:56:33 +0000

    Revamped and expanded BIT text system

--- lib/UserInterface/TextTable.h
+++ lib/UserInterface/TextTable.h
@@ -2,16 +2,53 @@
 #define TEXTTABLE_H
 #include <cstdint>
 
-//have to manally assign due to compiler limitations
-constexpr uint8_t H[5][5] = {
-    {1, 0, 0, 0, 1},
-    {1, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 1},
-    {1, 0, 0, 0, 1}
+//could be imporived by definingneach row
+constexpr uint8_t ROW_EMPTY[5] = {0, 0, 0, 0, 0};
+constexpr uint8_t ROW_FULL[5] = {1, 1, 1, 1, 1};
+
+//One bit maps
+constexpr uint8_t ROW_ONE_1[5] = {1, 0, 0, 0, 0};
+constexpr uint8_t ROW_ONE_2[5] = {0, 1, 0, 0, 0};
+constexpr uint8_t ROW_ONE_3[5] = {0, 0, 1, 0, 0};
+constexpr uint8_t ROW_ONE_4[5] = {0, 0, 0, 1, 0};
+constexpr uint8_t ROW_ONE_5[5] = {0, 0, 0, 0, 1};
+
+//Two bit maps
+constexpr uint8_t ROW_SIDES[5] = {1, 0, 0, 0, 1};
+
+const uint8_t* E[5] = {
+    ROW_FULL,
+    ROW_ONE_1,
+    ROW_FULL,
+    ROW_ONE_1,
+    ROW_FULL
 };
 
-const uint8_t (*text_table[128])[5] = { nullptr }; 
+const uint8_t* H[5] = {
+    ROW_SIDES,
+    ROW_SIDES,
+    ROW_FULL,
+    ROW_SIDES,
+    ROW_SIDES
+};
+
+const uint8_t* L[5] = {
+    ROW_ONE_1,
+    ROW_ONE_1,
+    ROW_ONE_1,
+    ROW_ONE_1,
+    ROW_FULL
+};
+
+const uint8_t* O[5] = {
+    ROW_FULL,
+    ROW_SIDES,
+    ROW_SIDES,
+    ROW_SIDES,
+    ROW_FULL
+};
+
+const uint8_t** text_table[128] = { nullptr }; 
 
 
 #endif
